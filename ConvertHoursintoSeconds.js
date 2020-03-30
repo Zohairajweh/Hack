9cf4875e6a7d1577f8260955f780cd38adbf3e9a
Zohairajweh <zohair.ajweh@hotmail.com>
2020-03-30 15:25:31 -0700

    howManySeconds js

---
+++ ConvertHoursintoSeconds.js
@@ -0,0 +1,5 @@
+function howManySeconds(hours) {
+	hours = (hours) * 60 * 60 ; 
+return hours;
+}
+console.log(howManySeconds);
--- js/Multipleof100.js
+++ js/Multipleof100.js
@@ -3,6 +3,7 @@ function divisible(num) {
 	return num%100 === 0? true: false;
 
 }
+console.log(divisible);
 // Create a function that takes an integer and returns true if it's divisible by 100, otherwise return false.
 
 //At least one of your solutions must include an arrow function
